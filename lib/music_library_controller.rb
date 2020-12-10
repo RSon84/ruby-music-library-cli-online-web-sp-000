bd26c5351a720e0836dd6ede793192ab11c9fc97
Devin Wilson <craigvin84@yahoo.com>
2020-12-10 06:15:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -130,7 +130,7 @@ class MusicLibraryController
   if (1..Song.all.length).include?(input)
     array = Song.all.sort{|a, b| a.name <=> b.name}
 
-      song = array.uniq[input+2]
+      song = array.uniq[input]
      puts "Playing #{song.name} by #{song.artist.name}" if song
 end
 end
